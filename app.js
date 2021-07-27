eef7e1f09aa1f6106943c0a94f274c6a49549a86
jlbirm <jlbirmingham14@gmail.com>
2021-07-27 15:20:09 -0500

    Added backspace and evaluation functions

--- app.js
+++ app.js
@@ -13,6 +13,45 @@ function updateDisplay(num){
     }
 }
 
+function backspace(currentDisplay){
+    str = currentDisplay.innerText.slice(0, -1);
+    if(str === ''){
+        str = '0';
+    }
+    currentDisplay.innerText = str;
+}
+
+function solveNums(currentDisplay){
+    const operators = ['−','+','×','÷'];
+    for(let o of operators){
+        if(currentDisplay.innerText.includes(o)){
+            let opIndex = currentDisplay.innerText.indexOf(o);
+            let x = Number(currentDisplay.innerText.slice(0, opIndex));
+            let y = Number(currentDisplay.innerText.slice(opIndex+1));
+
+            var num = 0;
+            switch (o){
+                case '−':
+                    num = x - y;
+                    break;
+                case '+':
+                    num = x + y;
+                    break;
+                case '×':
+                    num = x * y;
+                    break;
+                case '÷':
+                    num = x / y;
+                    break;
+            };
+
+        } else {
+            let num = Number(currentDisplay.innerText);
+        }
+    }
+    return(num);
+}
+
 // Click event for numerical & . buttons
 const numBtns = document.getElementsByClassName('numButton');
 for(let num of numBtns){
@@ -26,6 +65,11 @@ for(let num of numBtns){
 const opBtns = document.getElementsByClassName('opButton');
 for(let op of opBtns){
     op.addEventListener('click', function(){
+        let currentDisplay = document.getElementById('display');
+        // If last button pressed was an operand, replace with new operand
+        if(['.','−','+','×','÷'].includes(currentDisplay.innerText.slice(-1))){
+            backspace(currentDisplay);
+        }
         updateDisplay(op.innerText);
     })
 }
@@ -35,5 +79,20 @@ const clear = document.getElementById('clear');
 clear.addEventListener('click', function(){
     let currentDisplay = document.getElementById('display');
     currentDisplay.innerText = '0';
+})
+
+// Click event for backspace button
+const backspaceBtn = document.getElementById('backspace');
+backspaceBtn.addEventListener('click', function(){
+    let currentDisplay = document.getElementById('display');
+    backspace(currentDisplay);
+})
+
+// Click event for percentage button
+const percentBtn = document.getElementById('percentage');
+percentBtn.addEventListener('click', function(){
+    let currentDisplay = document.getElementById('display');
+    num = solveNums(currentDisplay);
+    currentDisplay.innerText = num/100;
 })
 
